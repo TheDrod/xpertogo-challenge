8f3b421c120169c3dd7d308b6b17116e79b144e9
TheDrod <palexdrod92@gmail.com>
2022-10-02 21:01:06 +0100

    Improve search input

--- src/app/components/search-input/search-input.component.ts
+++ src/app/components/search-input/search-input.component.ts
@@ -7,6 +7,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class SearchInputComponent {
   @Input() value: string = '';
+  @Input() placeholder: string = 'Search...';
 
   @Output() update: EventEmitter<string> = new EventEmitter<string>();
   @Output() search: EventEmitter<void> = new EventEmitter<void>();
