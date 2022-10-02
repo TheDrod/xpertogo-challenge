1c06da1a744daed9449f98787686e43d22520314
TheDrod <palexdrod92@gmail.com>
2022-10-02 09:47:32 +0100

    Add custom page title

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,40 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { Injectable, NgModule } from '@angular/core';
+import { RouterModule, RouterStateSnapshot, Routes, TitleStrategy } from '@angular/router';
 
 import { MoviesPageComponent } from './pages/movies-page/movies-page.component';
 import { FormPageComponent } from './pages/form-page/form-page.component';
+import { Title } from '@angular/platform-browser';
 // import { PageNotFoundComponent } from './pages/form-page/form-page.component';
 
 const routes: Routes = [
-  { path: 'movies', component: MoviesPageComponent },
-  { path: 'form', component: FormPageComponent },
+  { path: 'movies', component: MoviesPageComponent, title: 'Movies' },
+  { path: 'form', component: FormPageComponent, title: 'Form' },
   { path: '', redirectTo: '/movies', pathMatch: 'full' },
   // { path: '**', component: PageNotFoundComponent },
 ];
 
+@Injectable({ providedIn: 'root' })
+export class TemplatePageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+
+    if (title === undefined) {
+      return;
+    }
+
+    this.title.setTitle(`Xpertgo Challenge | ${title}`);
+  }
+}
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
+  providers: [
+    { provide: TitleStrategy, useClass: TemplatePageTitleStrategy },
+  ]
 })
 export class AppRoutingModule { }
